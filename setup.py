2ecedb55c7d321bffbdeae30982d04b30c5680f7
Huynh Minh Triet <100458317+Anshler@users.noreply.github.com>
2023-12-27 20:02:36 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vietnamese-poem-classifier",
+    version="0.1.0",
+    description="Classify genre and score Vietnamese poems",
+    url="https://github.com/Anshler/vietnamese-poem-classifier",
+    author="Huynh Minh Triet",
+    author_email="huynhminhtriet2002@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
+    ],
+    keywords="poem",
+    py_modules=['vietnamese_poem_classifier'],
+    install_requires=[
+        "transformers",
+        "importlib",
+        "importlib-resources",
+    ],
+    entry_points={
+        "console_scripts": ["vietnamese-poem-classifier=vietnamese_poem_classifier:main"],
+    },
+)
