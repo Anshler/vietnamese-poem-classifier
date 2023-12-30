14fbb01c2ee0165feec1a684e4808dfa92fdc81e
Huynh Minh Triet <huynhminhtriet2002@gmail.com>
2023-12-30 20:02:16 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="vietnamese-poem-classifier",
-    version="0.1.2",
+    version="0.1.3",
     description="Classify genre and score Vietnamese poems",
     url="https://github.com/Anshler/vietnamese-poem-classifier",
     author="Huynh Minh Triet",
